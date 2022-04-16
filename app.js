a2c3028428aa2d72b72c717d27d07d5d8279f7f0
RDHarold11 <74198960+RDHarold11@users.noreply.github.com>
2022-04-16 16:11:54 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+document.querySelector('.menu-btn').addEventListener('click', () =>{
+    document.querySelector('.nav-items').classList.toggle('show')
+});
+
+ScrollReveal().reveal('.content');
+ScrollReveal().reveal('.section', { delay: 500});
+ScrollReveal().reveal('.banner', { delay: 500});
+ScrollReveal().reveal('.testimonials', { delay: 500});
+ScrollReveal().reveal('.banner-envidia', { delay: 500});
+ScrollReveal().reveal('.products', { delay: 500});
+ScrollReveal().reveal('.banner-three', { delay: 500});
+ScrollReveal().reveal('.footer-items', { delay: 500});
